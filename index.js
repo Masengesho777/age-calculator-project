462a2285fe803af4c6d586d5266fe1a72d619f51
masengeshocadeau <masengeshocadeau603@gmail.com>
2023-10-10 20:06:50 +0200

    3rd commit

--- index.js
+++ index.js
@@ -11,8 +11,49 @@ let dob = new Date(), today = new Date();
 function calculate() {
     disblock .style.display = 'block'; 
     let dobVal = dobDate.Value;
-    console.log(dob);
-
-    
+    // console.log(dob);
+    let year, month, day;
+    day = (function () {
+        if (today.getDate() > dob.getDate()) {
+            return today.getDate() - dob.getDate() - 1   
+        }
+        else if (today.getDate() == dob.getDate()) {
+            return today.getDate() - dob.getDate()
+        }
+        else{
+            let calDate = new Date(dob.getFullYear(), dob.getMonth() + 1, 0);
+            return (today.getDate() + calDate.getDate()) - dob.getDate() - 1
+        }
+        
+        
+    }());
+   month = (function () {
+     if (today.getMonth() >= dob.getMonth()) {
+        if (today.getMonth() >= dob.getDate()) {
+            return today.getMonth() - dob.getDate();
+        }
+        else{
+            if ((today.getDate() - 1) >= dob.getMonth()) {
+               return (today.getMonth() -1) - dob.getMonth();
+            }
+            else{
+                return ((today.getMonth() - 1) + 12) - dob.getMonth();
+            }
+        }
+     }
+     else{
+        if (today.getDate() >= dob.getDate()) {
+            
+            return (today.getMonth() + 12) - dob.getMonth();
+        }
+        else{
+            return ((today.getMonth() - 1) + 12) - dob.getMonth();
+        }
+     }
+       
+        
+        
+   } ());
+    age.innerHTML = "you are <br>" + year + " Years," + month +   "Months," + day + "  Days"
 }
-calcBtn.onclick = calculate;
+    calcBtn.onclick = calculate;
